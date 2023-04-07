e9f85b7469b19bd4462cd870829dc1bf6130da9c
Abhishek Shah <abhishek.code@outlook.com>
2023-04-07 17:51:00 +0530

    implemented the game's core logic

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-'use strict';
+"use strict";
+
+const secretNumber = Math.trunc(Math.random() * 6) + 1;
+let score = 20;
+console.log(secretNumber);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  //When there is no input.
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No Number";
+
+    //When player wins.
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    //When guess is too high.
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    //When guess is too low.
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
